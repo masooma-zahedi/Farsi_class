c9f054e2a257b412b1ffd108ae3a66ccc56d64b4
masooma zahedi <masooma.zahedi1@gmail.com>
2024-10-09 17:55:46 -0500

    sentences for zad letter in word.html

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function putPic(id,url){
 function putWord(id,word){
     document.getElementById(id).innerHTML="<h1 class='enWord mt-5'>"+word+"</h1>";
  }
+
+ function putWordPic(id,word ,url){
+    if(word !== "" && url !== ""){
+        document.getElementById(id).innerHTML = "<div> <img class ='imgword' src='"+url+"'></div> <h1 class='enword text-light text-center mt-1'>"+ word+"</h1>"
+    } else if (word && url === "") {
+        putWord(id,word);
+    } else if(word === "" && url){
+        putPic(id,url)
+    }
+ }
