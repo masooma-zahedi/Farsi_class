3695218d55a9878aa87826b291a4e017c8e62d31
masooma zahedi <masooma.zahedi1@gmail.com>
2024-10-09 11:27:09 -0500

    start git for project

---
+++ index.js
@@ -0,0 +1,6 @@
+function putPic(id,url){
+    document.getElementById(id).innerHTML="<img class='imgWord' src='"+url+"'>";
+ }
+function putWord(id,word){
+    document.getElementById(id).innerHTML="<h1 class='enWord mt-5'>"+word+"</h1>";
+ }
