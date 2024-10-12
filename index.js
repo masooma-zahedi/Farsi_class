537ebf0604c531566cc4b4db759f1bd0b5170c08
masooma zahedi <masooma.zahedi1@gmail.com>
2024-10-12 12:41:31 -0500

    make page for sentencess.

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ function putWord(id,word){
         putPic(id,url)
     }
  }
+
+//  ******************************** word Section************
+function sentenceAll (id){
+    let x = document.getElementById(id)
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+}
