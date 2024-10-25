cf9f4d182beeb15d2b8505f87dd12113b7cbadf8
masooma zahedi <masooma.zahedi1@gmail.com>
2024-10-25 12:47:05 -0500

    add sond to all pic at alphabet example in alphabet page

--- index.js
+++ index.js
@@ -28,32 +28,35 @@ function sentenceAll(id){
     }
 }
                         // ******** Add Sentence To Carousel********
-    let sentences = [];
-    
-    renderHtml();
-    function renderHtml(){
-        let newSentence = "";
-        let Nsentence = "";
-        for(let i = 0; i < sentences.length; i++){
-            const getSentence = sentences[i];
-            if(i === 0){
-                const html = `<div  class="carousel-item active  pe-5">
-                      <h1 class="text-center">${getSentence} <button onclick='sentences.splice(${i},1); renderHtml();' style='background-color:transparent ;border:none; width:60px; height:50px;'><img class='w-100 h-100' style='overflow:hidden' src='./images/word_pic/10-22-1-deleteButton.webp'></button></h1>
-                    </div>`;
-                    newSentence +=html;
-            } else {                
-                // const getSentence = sentences[i];
-                const html = `<div  class="carousel-item pe-5">
-                      <h1 class="text-center">${getSentence} <button onclick='sentences.splice(${i},1); renderHtml();' style='background-color:transparent ;border:none; width:60px; height:50px;'><img class='w-100 h-100' style='overflow:hidden' src='./images/word_pic/10-22-1-deleteButton.webp'></button></h1>
-                    </div>`;
-                    // renderHtml();
-                    newSentence +=html;
-                }            
-                const sentenceHtml = `<h2>${getSentence}</h2>`;
-                Nsentence += sentenceHtml;
+    if(!alphabetPage){
+        let sentences = [];
+        
+        renderHtml();
+        function renderHtml(){
+            let newSentence = "";
+            let Nsentence = "";
+            for(let i = 0; i < sentences.length; i++){
+                const getSentence = sentences[i];
+                if(i === 0){
+                    const html = `<div  class="carousel-item active  pe-5">
+                          <h1 class="text-center">${getSentence} <button onclick='sentences.splice(${i},1); renderHtml();' style='background-color:transparent ;border:none; width:60px; height:50px;'><img class='w-100 h-100' style='overflow:hidden' src='./images/word_pic/10-22-1-deleteButton.webp'></button></h1>
+                        </div>`;
+                        newSentence +=html;
+                } else {                
+                    // const getSentence = sentences[i];
+                    const html = `<div  class="carousel-item pe-5">
+                          <h1 class="text-center">${getSentence} <button onclick='sentences.splice(${i},1); renderHtml();' style='background-color:transparent ;border:none; width:60px; height:50px;'><img class='w-100 h-100' style='overflow:hidden' src='./images/word_pic/10-22-1-deleteButton.webp'></button></h1>
+                        </div>`;
+                        // renderHtml();
+                        newSentence +=html;
+                    }            
+                    const sentenceHtml = `<h2>${getSentence}</h2>`;
+                    Nsentence += sentenceHtml;
+            }
+            document.querySelector("#letterGh2").innerHTML = newSentence;
+            document.querySelector("#letterGh4").innerHTML = Nsentence;
         }
-        // document.querySelector("#letterGh2").innerHTML = newSentence;
-        // document.querySelector("#letterGh4").innerHTML = Nsentence;
+
     }
 
 
@@ -140,13 +143,32 @@ function removeWord(id){
 
 }
 
-// setInterval(changebg(),3000)
-
-// changebg();
 setInterval(function changebg(){
     let bgColor = [ "rgb(125, 48, 8)", " rgb(135, 48,88)","rgb(101, 48, 75)"," rgb(59, 18, 148)","rgb(84, 11, 11)","rgb(21, 86, 86)"];
-    let color = bgColor[Math.floor(Math.random()*bgColor.length)]
+    let color = bgColor[Math.floor(Math.random()*bgColor.length)];
     document.querySelector(".changeColor").style.backgroundColor = color;
-    console.log(color);
     
-},1000)
+},20000);
+
+function test(id,title,q,w,e,r="",t="",y="",u="",i="",o="",p=""){
+    console.log(id);
+    
+    document.querySelector(id).innerHTML=`<section id="" class="container-fluid">
+          <div class="p-4 pageN1 w-50" style="background-color: rgb(215, 236, 250);">
+            <h1 class="text-center  p-4 rounded-3 text-light" style="background-color: rgb(29, 26, 159);">${title}</h1>
+            <h2>${q}</h2>
+            <h2> ${w} </h2>
+            <h2> ${e} </h2>
+            <h2> ${r} </h2>
+            <h2> ${t} </h2>
+            <h2> ${y} </h2>
+            <h2> ${u} </h2>
+            <h2> ${i} </h2>
+            <h2> ${o} </h2>
+            <h2> ${p} </h2>
+          </div>
+        </section>
+`;
+console.log(id);
+
+}
